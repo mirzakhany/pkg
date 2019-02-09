83104e7c79856547d5930d6b4eefb990ac0bccd4
mohsen <mohsenmirzakhany@gmail.com>
2019-02-09 13:04:52 +0330

    rename security files and add comments

--- security/security.go
+++ security/security.go
@@ -2,11 +2,13 @@ package security
 
 import "golang.org/x/crypto/bcrypt"
 
+// HashPassword hash the password
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
 }
 
+// CheckPasswordHash check password against hash
 func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
